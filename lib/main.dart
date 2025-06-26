3ae51b56e10258074c0484d17ca9540bbe989ca1
TehminaFatima <tehmina.sgr@gmail.com>
2025-06-26 22:42:25 +0500

    packages added

--- lib/main.dart
+++ lib/main.dart
@@ -2,10 +2,7 @@ import 'package:flutter/material.dart';
 import 'wake_me_here.dart';
 import 'package:flutter_dotenv/flutter_dotenv.dart';
 
-// Future<void> main() async {
-//   await dotenv.load();
-//   runApp(WakeMeHereApp());
-// }
-void main() {
+void main() async {
+  await dotenv.load();
   runApp(WakeMeHereApp());
 }
